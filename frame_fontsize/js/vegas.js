52a6e1f839c455cce02c4c085fffae129276d52d
MOBnD <119469004+MOBnD@users.noreply.github.com>
2025-02-02 20:40:58 +0900

    Update vegas.js

--- frame_fontsize/js/vegas.js
+++ frame_fontsize/js/vegas.js
@@ -7,7 +7,7 @@ $(function() {
         ],
 		transition: 'blur',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
 		animation: 'kenburns',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
-		delay: 6000,				//次の画像を表示するまでの時間
+		delay: 1,				//次の画像を表示するまでの時間
 		animationDuration: 10000,	//アニメーション間の引き継ぎタイミング。
     });
 });
